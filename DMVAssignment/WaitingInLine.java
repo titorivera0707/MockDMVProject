3e060a77504194365aeebfe321691debcc937193
Tito Rivera <titorivera0707@gmail.com>
2022-11-15 16:31:57 -0800

    Working on the first user option. The first two print lines print together rather than one by one.

--- DMVAssignment/WaitingInLine.java
+++ DMVAssignment/WaitingInLine.java
@@ -8,38 +8,38 @@ public class WaitingInLine {
         Scanner keyboard = new Scanner (System.in);
 
         System.out.println("Hello, Choose An Option.");
-        System.out.println("1 - Register a Car\n2 - Search a Car\nPrint Report\n4 - Exit\n\n");
-        System.out.println("Please enter your choice: ");
+        System.out.println("1 - Register a Car\n2 - Search a Car\n3 - Print Report\n4 - Exit\n");
+        System.out.print("Please enter your choice: ");
         int userNumber = keyboard.nextInt();
         while(userNumber < 1 || userNumber >4) {
-            System.out.println("Please use a valid number 1-4");
+            System.out.print("Please use a valid number 1-4");
             userNumber = keyboard.nextInt();
         }
 
-        if (userNumber = 1) {
-            Person newPerson = new Person();
-            Car carAdd = new Car();
-            System.out.println("Enter name: ");
+        if (userNumber == 1) {
+            Person newPerson = new Person(null, null, null);
+            Car carAdd = new Car(null, null, null);
+            System.out.print("Enter name: ");
             newPerson.setName(keyboard.nextLine());
-            System.out.println("Enter email: ");
+            System.out.print("Enter email: ");
             newPerson.setEmail(keyboard.nextLine());
-            System.out.println("Phone number: ");
+            System.out.print("Phone number: ");
             newPerson.setPhoneNumber(keyboard.nextLine());
 
-            System.out.println("Car Make : ");
+            System.out.print("Car Make : ");
             carAdd.setMake(keyboard.nextLine());
-            System.out.println("Car Model : ");
+            System.out.print("Car Model : ");
             carAdd.setModel(keyboard.nextLine());
-            System.out.println("Car Color : ");
+            System.out.print("Car Color : ");
             carAdd.setColor(keyboard.nextLine());
-            
+
         }
 
-        else if(userNumber = 2) {
+        else if(userNumber == 2) {
             //Search for a car
         }
 
-        else if(userNumber = 3) {
+        else if(userNumber == 3) {
             //Print DMV report
         }
 
@@ -47,6 +47,7 @@ public class WaitingInLine {
             System.exit(0);
         }
 
+        keyboard.close();
     }
 
 }
